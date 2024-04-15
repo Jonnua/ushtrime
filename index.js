f77a8172d4330084e97b1af5925cc7e6f340cc9c
Jonnua <jonarrahmani4@gmail.com>
2024-04-15 17:52:29 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+// ReactDOM.render(
+// <div>
+//     <h1>react course</h1>
+//     <p>we will learn react in this course</p>
+// </div>,
+//     document.getElementById("root")
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+<div>
+    <h1>react course</h1>
+    <p>we will learn react in this course</p>
+</div>
+
+//kodi i krijum nga babel te profa 
+    // React.createElement(
+    //     "div",
+    //     null,
+    //     React.createElement("h1", null, "React Course1"),
+    //     React.createElement(
+    //         "p",
+    //         null,
+    //         "We will learn react in this course."
+    //     )
+    // )
+
+
+
+
+//kodi qe mu ka 
+    // import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+    // /*#__PURE__*/_jsxs("div", {
+    //   children: [/*#__PURE__*/_jsx("h1", {
+    //     children: "react course"
+    //   }), /*#__PURE__*/_jsx("p", {
+    //     children: "we will learn react in this course"
+    //   })]
+    // });
+);
