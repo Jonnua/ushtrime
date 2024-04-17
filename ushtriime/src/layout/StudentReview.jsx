84f6bf5d21ce9ea4d5f2b3f50579015cb9b301f0
Jonnua <jonarrahmani4@gmail.com>
2024-04-17 17:56:42 +0200

    Create StudentReview.jsx

---
+++ ushtriime/src/layout/StudentReview.jsx
@@ -0,0 +1,10 @@
+export default function StudentReview() {
+    return (
+      <div className="p-2">
+        
+        <i className="bi bi-hand-thumbs-up text-success p-1"></i>
+        <i className="bi bi-hand-thumbs-down text-danger p-1"></i>
+      </div>
+    );
+  }
+  
