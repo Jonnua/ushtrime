44e98bed771f5af106fd3bc658247f3f43bad8f3
Jonnua <jonarrahmani4@gmail.com>
2024-04-17 17:56:35 +0200

    Update Student.jsx

--- ushtriime/src/Student.jsx
+++ ushtriime/src/Student.jsx
@@ -1,19 +1,22 @@
-import {faker} from '@faker-js/faker';
+//import {faker} from '@faker-js/faker';
 
 export default function Student(props) {//props perdoret per me i bart atributet t ni funksioni
     // const fullName = "Olti Miftari";
     // const programmingExp = 2;
     return (
-      <div className="container p-4">
+      <div className="col-4 p-1">
         <div className="row border">
-          <div className="col-1">
-            <img src={faker.image.avatar()} className="w-100" alt="avatar" />
+          <div className="col-2">
+            <img src={props.headshot} className="w-100 py-2"  />
           </div>
-          <div className="col-10">
+          <div className="col-8">
             {props.name}
             <br />
-            programming experience {props.experience} years
+            Coding experience {props.experience} years
           </div>
+          <div className="col-2">
+            {props.children}
+            </div>
         </div>
       </div>
     );
