91209e9d020307b449c8adf17d4debceed1a6eb7
Jonnua <jonarrahmani4@gmail.com>
2024-04-17 12:10:48 +0200

    Update Student.jsx

--- ushtriime/src/Student.jsx
+++ ushtriime/src/Student.jsx
@@ -1,3 +1,5 @@
+import {faker} from '@faker-js/faker';
+
 export default function Student(props) {//props perdoret per me i bart atributet t ni funksioni
     // const fullName = "Olti Miftari";
     // const programmingExp = 2;
@@ -5,7 +7,7 @@ export default function Student(props) {//props perdoret per me i bart atributet
       <div className="container p-4">
         <div className="row border">
           <div className="col-1">
-            <img src={`https://ui-avatars.com/api/?name=${props.name}`} className="w-100" alt="avatar" />
+            <img src={faker.image.avatar()} className="w-100" alt="avatar" />
           </div>
           <div className="col-10">
             {props.name}
